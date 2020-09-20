df4097e3c9a184fac5a54db858f1930732a28290
Wei Chen <wweic@amazon.com>
2020-09-20 00:06:36 -0700

    Add hs.tiling

--- init.lua
+++ init.lua
@@ -55,6 +55,21 @@ for _, v in pairs(hspoon_list) do
     hs.loadSpoon(v)
 end
 
+local tiling = require "hs.tiling"
+local hotkey = require "hs.hotkey"
+local mash = {"ctrl", "cmd"}
+
+hotkey.bind(mash, "c", function() tiling.cycleLayout() end)
+hotkey.bind(mash, "j", function() tiling.cycle(1) end)
+hotkey.bind(mash, "k", function() tiling.cycle(-1) end)
+hotkey.bind(mash, "space", function() tiling.promote() end)
+hotkey.bind(mash, "f", function() tiling.goToLayout("fullscreen") end)
+
+-- If you want to set the layouts that are enabled
+tiling.set('layouts', {
+  'fullscreen', 'main-vertical'
+})
+
 ----------------------------------------------------------------------------------------------------
 -- Then we create/register all kinds of modal keybindings environments.
 ----------------------------------------------------------------------------------------------------
